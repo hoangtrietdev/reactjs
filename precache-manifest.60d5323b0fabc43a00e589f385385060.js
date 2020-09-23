61b19f757af826d1cca39b9860c0a2045a5d166d
Triet <abc@iMacTriet.local>
2020-09-23 21:01:55 +0700

    Updates

---
+++ precache-manifest.60d5323b0fabc43a00e589f385385060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20cd3afc023c700fd57e8e2cd300e65",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "6a3a2d2617a12e805960",
+    "url": "/reactjs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/reactjs/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3a2d2617a12e805960",
+    "url": "/reactjs/static/js/main.52be67a2.chunk.js"
+  },
+  {
+    "revision": "637e33110a0b4e07666c",
+    "url": "/reactjs/static/js/runtime-main.76d7fa02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
